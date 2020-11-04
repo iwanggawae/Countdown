b145e6f615a5c3493de79874d9c17debd73a5b7c
Tetsing <iwanggawae@gmail.com>
2020-11-04 16:13:42 +0700

    File Asli!

--- Countdown/ViewController.swift
+++ Countdown/ViewController.swift
@@ -6,11 +6,64 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var seconds = 30
+    var timer = Timer ()
+    var sfx = AVAudioPlayer()
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var sliderOutlet: UISlider!
+    @IBAction func slider(_ sender: UISlider)
+    {
+        seconds = Int (sender.value )
+        label.text = String(seconds) + "seconds"
+    }
+    
+   
+    
+    
+    @IBOutlet weak var startOutlet: UIButton!
+    @IBAction func start(_ sender: Any)
+    {
+       timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true )
+        sliderOutlet.isHidden = true
+        startOutlet.isHidden = true
+    }
+    
+    @objc func counter ()
+    {
+        seconds -= 1
+        label.text = String(seconds) + "seconds"
+        
+        if (seconds == 0)
+        {
+            timer.invalidate()
+            sfx.play()
+        }
+    }
+    @IBOutlet weak var stopOutlet: UIButton!
+    @IBAction func stop(_ sender: Any)
+    {
+        timer.invalidate()
+        seconds = 30
+        sliderOutlet.setValue(30, animated: true)
+        label.text = "30 seconds"
+        
+        sliderOutlet.isHidden = false
+        startOutlet.isHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        do
+        {
+            sfx = try! AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "notKlik", ofType: "mp3")!))
+        }
         // Do any additional setup after loading the view.
     }
 
